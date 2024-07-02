6551e218ca5ca88bcdf50a1765da900e8d48a6ab
Vaibhav <Vaibhav_pandey@outlook.in>
2024-07-02 14:03:56 +0530

    first commit

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="w-full h-[calc(100dvh-60px)] text-white flex justify-center items-center flex-col gap-3">
+      <h1 className="text-2xl md:text-6xl font-bold text-center">WEB COMPILER</h1>
+      <p className=" text-2xl gray-500 text-center">
+        COMPILE YOUR HTML,CSS,JAVASCRIPT CODE FOR FREE
+      </p>
+    </div>
+  );
+}
